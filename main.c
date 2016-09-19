7ebcf38a38146141cfdcf0ddb55148f5173d1ae2
GrooveStomp <aaron.oman@gmail.com>
2016-09-19 10:43:16 -0700

    Chapter 8: Error Handling

--- main.c
+++ main.c
@@ -8,27 +8,131 @@
 #include "gs.h"
 #include "mpc.h"
 
-long
-EvalOperator(long A, char *Operator, long B)
+typedef struct
 {
-        if(strcmp(Operator, "+") == 0) { return A + B; }
-        if(strcmp(Operator, "-") == 0) { return A - B; }
-        if(strcmp(Operator, "*") == 0) { return A * B; }
-        if(strcmp(Operator, "/") == 0) { return A / B; }
-        return(0);
+        int Type;
+        int Number;
+        int Error;
+} lval;
+
+enum lval_type_e
+{
+        LVAL_TYPE_NUMBER,
+        LVAL_TYPE_ERROR
+};
+
+enum lval_error_e
+{
+        LVAL_ERROR_DIV_ZERO,
+        LVAL_ERROR_BAD_OPERATOR,
+        LVAL_ERROR_BAD_NUMBER
+};
+
+lval
+LvalNumber(long Number)
+{
+        lval Result;
+        Result.Type = LVAL_TYPE_NUMBER;
+        Result.Number = Number;
+        return(Result);
 }
 
-long
+lval
+LvalError(int Error)
+{
+        lval Result;
+        Result.Type = LVAL_TYPE_ERROR;
+        Result.Error = Error;
+        return(Result);
+}
+
+void
+LvalPrint(lval Value)
+{
+        switch(Value.Type)
+        {
+                case(LVAL_TYPE_NUMBER):
+                {
+                        printf("%i", Value.Number);
+                } break;
+                case(LVAL_TYPE_ERROR):
+                {
+                        switch(Value.Error)
+                        {
+                                case(LVAL_ERROR_DIV_ZERO):
+                                {
+                                        printf("Error: Division By Zero!");
+                                } break;
+                                case(LVAL_ERROR_BAD_OPERATOR):
+                                {
+                                        printf("Error: Invalid Operator!");
+                                } break;
+                                case(LVAL_ERROR_BAD_NUMBER):
+                                {
+                                        printf("Error: Invalid Number!");
+                                } break;
+                        }
+                } break;
+        }
+}
+
+void
+LvalPrintln(lval Value)
+{
+        LvalPrint(Value); putchar('\n');
+}
+
+lval
+EvalOperator(lval A, char *Operator, lval B)
+{
+        lval Result;
+
+        if(A.Type == LVAL_TYPE_ERROR) { return(A); }
+        if(B.Type == LVAL_TYPE_ERROR) { return(B); }
+
+        if(strcmp(Operator, "+") == 0) { return(LvalNumber(A.Number + B.Number)); }
+        if(strcmp(Operator, "-") == 0) { return(LvalNumber(A.Number - B.Number)); }
+        if(strcmp(Operator, "*") == 0) { return(LvalNumber(A.Number * B.Number)); }
+        if(strcmp(Operator, "/") == 0) {
+                if(B.Number == 0)
+                {
+                        Result = LvalError(LVAL_ERROR_DIV_ZERO);
+                        return(Result);
+                }
+                else
+                {
+                        Result = LvalNumber(A.Number / B.Number);
+                        return(Result);
+                }
+        }
+
+        Result = LvalError(LVAL_ERROR_BAD_OPERATOR);
+        return(Result);
+}
+
+lval
 Eval(mpc_ast_t *Tree)
 {
+        lval Result;
+
         if(strstr(Tree->tag, "number"))
         {
-                return(atoi(Tree->contents));
+                errno = 0;
+                long Number = strtol(Tree->contents, NULL, 10);
+                if(errno == ERANGE)
+                {
+                        Result = LvalError(LVAL_ERROR_BAD_NUMBER);
+                }
+                else
+                {
+                        Result = LvalNumber(Number);
+                }
+
+                return(Result);
         }
 
         char *Operator = Tree->children[1]->contents;
-
-        long Parameter = Eval(Tree->children[2]);
+        lval Parameter = Eval(Tree->children[2]);
 
         int i = 3;
         while(strstr(Tree->children[i]->tag, "expr"))
@@ -74,14 +178,14 @@ main(int ArgCount, char *Arguments[])
 
         mpc_result_t r;
 
-        while(1)
+        while(true)
         {
-                char *input = readline("lispy> ");
-                add_history(input);
-                if(mpc_parse("<stdin>", input, Lispy, &r))
+                char *Input = readline("lispy> ");
+                add_history(Input);
+                if(mpc_parse("<stdin>", Input, Lispy, &r))
                 {
-                        long Result = Eval(r.output);
-                        printf("%li\n", Result);
+                        lval Result = Eval(r.output);
+                        LvalPrintln(Result);
                         mpc_ast_delete(r.output);
                 }
                 else
@@ -89,8 +193,7 @@ main(int ArgCount, char *Arguments[])
                         mpc_err_print(r.error);
                         mpc_err_delete(r.error);
                 }
-                printf("%s\n", input);
-                free(input);
+                free(Input);
         }
 
         mpc_cleanup(4, Number, Operator, Expr, Lispy);
